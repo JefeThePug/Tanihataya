fbb5e5bd03c563faa9f2ccd9674894b562754a5f
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2025-09-19 13:59:25 +0900

    Sprint Setup

---
+++ group/src/main/java/com/example/group/TanihatayaApplication.java
@@ -0,0 +1,13 @@
+package com.example.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanihatayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanihatayaApplication.class, args);
+	}
+
+}
