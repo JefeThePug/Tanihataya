8f5e2bbe915ec9521a743b7484e25d8f2a3bbf65
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2025-09-24 10:21:01 +0900

    ItemServiceとUserServiceの

--- group/src/main/java/com/example/group/service/UserService.java
+++ group/src/main/java/com/example/group/service/UserService.java
@@ -1,5 +1,35 @@
 package com.example.group.service;
 
-public class UserService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserService {
+	private final UserMapper userMapper;
+
+	List<Users> findAll() {
+		return userMapper.findAll();
+	}
+
+	Users findById(Integer userId) {
+		return userMapper.findById(userId);
+	}
+
+	void insert(Users user) {
+		userMapper.insert(user);
+	}
+
+	void update(Users user) {
+		userMapper.update(user);
+	}
+
+	void delete(Integer userId) {
+		userMapper.delete(userId);
+	}
 }
