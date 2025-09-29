20b0398e54d56279daba3c854a493a93e5fb7f85
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2025-09-29 15:34:11 +0900

    Update UserService.java

--- group/src/main/java/com/example/group/service/UserService.java
+++ group/src/main/java/com/example/group/service/UserService.java
@@ -5,11 +5,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.group.Entity.Users;
-<<<<<<< HEAD
 import com.example.group.form.UserForm;
-=======
 import com.example.group.repository.UsersMapper;
->>>>>>> 2e1a1830084d2b7a3eb46b6c93111e32d5f88577
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,38 +15,38 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserService {
 	private final UsersMapper userMapper;
-    private final PasswordEncoder passwordEncoder;
+	private final PasswordEncoder passwordEncoder;
 
 	public Users findByEmail(String email) {
 		return userMapper.findByEmail(email).get(0);
 	}
-	
+
 	public Users findById(Integer userId) {
 		return userMapper.findById(userId).get(0);
 	}
-	
+
 	public Users findByName(String username) {
 		return userMapper.findByName(username).get(0);
 	}
 
 	public void insert(UserForm form) {
-		  Users user = new Users();
-	        // フォームの生パスワードをハッシュ化
-	        String encodedPassword = passwordEncoder.encode(form.getPassword());
-	        
-	        user.setUserId(form.getUserId());
-	        user.setName(form.getName());
-	        user.setEmail(form.getEmail());
-	        user.setPassword(encodedPassword); // ハッシュ化済みパスワードをセット
-	        user.setPostcode(form.getPostcode());
-	        user.setAddress(form.getAddress());
-	        user.setTel(form.getTel());
-	        
-	        userMapper.insert(user);
-	    }
+		Users user = new Users();
+		// フォームの生パスワードをハッシュ化
+		String encodedPassword = passwordEncoder.encode(form.getPassword());
+
+		user.setUserId(form.getUserId());
+		user.setName(form.getName());
+		user.setEmail(form.getEmail());
+		user.setPassword(encodedPassword); // ハッシュ化済みパスワードをセット
+		user.setPostcode(form.getPostcode());
+		user.setAddress(form.getAddress());
+		user.setTel(form.getTel());
+
+		userMapper.insert(user);
+	}
 
 	public void update(UserForm user) {
-		userMapper.update(user);
+		//userMapper.update(user);
 	}
 
 	public void delete(Integer userId) {
