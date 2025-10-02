2183e9a62bfff9d4ba277966596c484fc69a2f82
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2025-10-02 09:48:05 +0900

    UserFormとUsersの直し

--- group/src/main/java/com/example/group/entity/Users.java
+++ group/src/main/java/com/example/group/entity/Users.java
@@ -2,10 +2,6 @@ package com.example.group.entity;
 
 import java.time.LocalDateTime;
 
-import jakarta.validation.constraints.AssertTrue;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -20,18 +16,14 @@ public class Users {
 
 	private String email;
 
-	@Size(max = 12, message = "パスワードは12文字以内で入力してください")
 	private String password;
 
-	@Pattern(regexp = "^[0-9]{7}$", message = "郵便番号はハイフンなしの7桁の数字で入力してください")
 	private String postcode;
 
 	private String address;
 
-	@Pattern(regexp = "^[0-9]{10,11}$", message = "電話番号はハイフンなしの10～11桁の数字で入力してください")
 	private String tel;
 
-	@AssertTrue(message = "アカウントが有効である必要があります")
 	private boolean isActive;
 
 	private LocalDateTime createdAt;
