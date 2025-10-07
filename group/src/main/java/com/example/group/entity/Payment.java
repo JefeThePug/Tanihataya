ad5f900582fe69cc7f003642b341c867a5e1d705
yamasaki-m-0328 <yamasaki.m.0328@gmail.com>
2025-10-07 13:10:34 +0900

    Payment

--- group/src/main/java/com/example/group/entity/Payment.java
+++ group/src/main/java/com/example/group/entity/Payment.java
@@ -2,9 +2,12 @@ package com.example.group.entity;
 
 import java.util.Date;
 
+import jakarta.persistence.Transient;
 import jakarta.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.CreditCardNumber;
+
+import com.example.group.form.PaymentForm;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,17 +17,29 @@ import lombok.NoArgsConstructor;
 public class Payment {
 
 	private Integer cardId;
-	
+
 	private Integer userId;
-	
+
 	@CreditCardNumber(message = "正しいカード番号を入力してください")
 	private String cardNumber;
-	
+
 	private String name;
-	
+
 	@Pattern(regexp = "\\d{3}", message = "セキュリティコードは3桁の数字で入力してください")
 	private String securityCode;
 
 	private Date expDate;
 	
+	 @Transient
+	 private boolean saveCardInfo;
+
+	public Payment(PaymentForm form) { 
+		this.cardId = form.getCardId(); 
+		this.userId = form.getUserId(); 
+		this.cardNumber = form.getCardNumber(); 
+		this.name = form.getName(); 
+		this.securityCode = form.getSecurityCode();
+		this.expDate = form.getExpDate(); 
+		this.saveCardInfo = form.isSaveCardInfo(); 
+	}
 }
